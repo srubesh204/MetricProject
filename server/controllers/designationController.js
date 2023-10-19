503302426286866adf71edfd8bc7cb38eee59710
Sridevi1996 <sridevi10011996@gmail.com>
2023-10-19 11:58:23 +0530

    added employee Route

--- server/controllers/designationController.js
+++ server/controllers/designationController.js
@@ -1,6 +1,5 @@
 const designationModel = require("../models/designationModel")
 
-<<<<<<< HEAD
 const designationController = {
     getAllDesignation: async (req, res) => {
         try {
@@ -80,30 +79,3 @@ const designationController = {
       }
       }
 module.exports = designationController;
-=======
-const designationControllers = {
-    getAllDesignation: async (req, res) => {
-        try {
-            const designationResult = await designationModel.find();
-            res.status(200).json(designationResult);
-            //res.status(200).json(employees);
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Error on Designation');
-        }
-    },
-    createDesignation: async (req, res) => {
-
-        try {
-            const { designation } = req.body;
-            const designationResult = new designationModel({ designation });
-            await designationResult.save();
-            res.status(201).json(designationResult);
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: 'Internal server error on Designation' });
-        }
-    }
-}
-module.exports = designationControllers;
->>>>>>> origin
--- server/routes/departmentRoute.js
+++ server/routes/departmentRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const departmentController = require('../controllers/departmentController')
 const router = express.Router();
@@ -7,16 +6,5 @@ router.get("/getDepartment", departmentController.getDepartment)
 router.post("/createDepartment", departmentController.createDepartment)
 router.put("/updateDepartment/:id", departmentController.updateDepartment)
 router.delete("/deleteDepartment/:id", departmentController.deleteDepartment)
-=======
-const express = require('express');
-const departmentController = require('../controllers/departmentController')
-const router = express.Router();
-
-
-router.get('/getAllDepartments', departmentController.getDepartment);
-router.post('/createDepartment', departmentController.createDepartment);
-
-
->>>>>>> origin
 
 module.exports = router;
