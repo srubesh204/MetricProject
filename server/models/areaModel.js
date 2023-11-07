bae0795ab7c044a89d5fab9df4129023ebc1e4ec
srubesh204 <srubesh204@gmail.com>
2023-11-07 11:41:58 +0530

    done

--- server/models/areaModel.js
+++ server/models/areaModel.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 //const uniqueValidator = require('mongoose-unique-validator');
 
 const areaSchema = new mongoose.Schema({
-<<<<<<< HEAD
+
   area: {
     type: String,
     
+  },
+  areaStatus: {
+    type: String
   }
-=======
-  area: String,
-  areaStatus:String,
->>>>>>> 7244357623c0b6b16707843eb531937d08bf8be2
+
     
 });
 //unitSchema.plugin(uniqueValidator);
