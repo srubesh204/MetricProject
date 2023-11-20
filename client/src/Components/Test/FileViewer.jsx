d83a5e63f5e6ba4e14d0ba3973e2eba205a5a135
srubesh204 <srubesh204@gmail.com>
2023-11-20 09:13:39 +0530

    mm

--- client/src/Components/Test/FileViewer.jsx
+++ client/src/Components/Test/FileViewer.jsx
@@ -1,75 +1,13 @@
-import { Box, Link } from '@mui/material';
-import axios from 'axios';
-import React, { useState, useRef, useEffect } from 'react';
-import { PieChart } from '@mui/x-charts';
-import { DataGrid } from '@mui/x-data-grid';
-import dayjs from 'dayjs';
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
 
-
-
-const DateAndMonthsInput = () => {
-  const [inputData, setInputData] = useState({
-    selectedDate: '',
-    selectedMonths: '',
-    resultDate: '',
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setInputData({
-      ...inputData,
-      [name]: value,
-    });
-
-    if (name === 'selectedMonths') {
-      calculateResultDate(inputData.selectedDate, value);
-    } else if (name === 'selectedDate') {
-      calculateResultDate(value, inputData.selectedMonths);
-    }
-  };
-
-  const calculateResultDate = (inputDate, months) => {
-    const parsedDate = dayjs(inputDate);
-    if (parsedDate.isValid() && !isNaN(months)) {
-      const calculatedDate = parsedDate.add(parseInt(months, 10), 'month').subtract(1, 'day');
-      setInputData({
-        ...inputData,
-        selectedDate: inputDate,
-        selectedMonths: months,
-        resultDate: calculatedDate.format('YYYY-MM-DD'),
-      });
-    }
-  };
-
+export default function BasicBars() {
   return (
-    <div>
-      <label>
-        Date Input:
-        <input
-          type="date"
-          name="selectedDate"
-          value={inputData.selectedDate}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Months to Add:
-        <input
-          type="number"
-          name="selectedMonths"
-          value={inputData.selectedMonths}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Result Date:
-        <input type="text" value={inputData.resultDate} readOnly />
-      </label>
-    </div>
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: ['Total Items', 'Active', 'Spare', 'Break Down', 'Missing', 'Rejection'] }]}
+      series={[{ data: [4, 3, 5, 7, 8, 10] }]}
+      width={500}
+      height={300}
+    />
   );
-};
-
-export default DateAndMonthsInput;
-
+}
