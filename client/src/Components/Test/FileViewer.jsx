50e57880f3355d2a6775a1f796cd5eb2a3f4637e
srubesh204 <srubesh204@gmail.com>
2023-11-21 12:22:49 +0530

    mm

--- client/src/Components/Test/FileViewer.jsx
+++ client/src/Components/Test/FileViewer.jsx
@@ -1,13 +1,19 @@
-import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import React from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+const FileViewer = () => {
 
-export default function BasicBars() {
+ 
+
+  
+
+
   return (
-    <BarChart
-      xAxis={[{ scaleType: 'band', data: ['Total Items', 'Active', 'Spare', 'Break Down', 'Missing', 'Rejection'] }]}
-      series={[{ data: [4, 3, 5, 7, 8, 10] }]}
-      width={500}
-      height={300}
-    />
-  );
+    <div></div> 
+  )
 }
+
+export default FileViewer
+
+
+
+                            
