7cab3c9252d673cfe10dccdd7690035323c6a3a2
Sridevi1996 <sridevi10011996@gmail.com>
2023-12-26 14:56:35 +0530

    ff

--- client/src/Components/Instrument_History_Card.jsx
+++ client/src/Components/Instrument_History_Card.jsx
@@ -3,19 +3,45 @@ import { Container, TextField, MenuItem, Paper, Button } from "@mui/material";
 import { AdapterDayjs, } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { DataGrid } from '@mui/x-data-grid';
+import { useEffect } from "react";
+import axios from 'axios';
+
 
 const rows = [
-    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 1, col1: 'Hello', col2: 'World', col3: 'Everyone', col4: 'Everyone', col5: 'Everyone', col6: 'Everyone', col7: 'Everyone', col8: 'Everyone' },
     // Add more rows as needed
   ];
   
   const columns = [
     { field: 'col1', headerName: 'SlNo', width: 150 },
     { field: 'col2', headerName: 'Calibration Date', width: 150 },
+    { field: 'col3', headerName: 'Calibration Status', width: 150 },
+    { field: 'col4', headerName: 'Next calibration Date', width: 150 },
+    { field: 'col5', headerName: 'Certificate Status', width: 150 },
+    { field: 'col6', headerName: 'Certificate No', width: 150 },
+    { field: 'col7', headerName: 'Observed Size 1', width: 150 },
+    { field: 'col8', headerName: 'Calibrated At', width: 150 },
+    
     // Add more columns as needed
   ];
+
   
 function Instrument_History_Card() {
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(
+            `${process.env.REACT_APP_PORT}/itemCal/getAllItemCals`
+          );
+          console.log(response.data); // Handle the response data as needed
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData(); // Call the function to fetch data when the component mounts
+    }, []);
+    
     return (
         <div>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -52,7 +78,7 @@ function Instrument_History_Card() {
                                     </TextField>
                                 </div>
 
-                                <div className="col-1 offset-5">
+                                <div className="col-2 offset-3">
                                     <DatePicker
                                         fullWidth
                                         id="fromDateId"
@@ -62,7 +88,7 @@ function Instrument_History_Card() {
                                         slotProps={{ textField: { size: 'small' } }}
                                         format="DD-MM-YYYY" />
                                 </div>
-                                <div className="col-1">
+                                <div className="col-2">
                                     <DatePicker
                                         fullWidth
                                         id="toDateId"
@@ -74,24 +100,24 @@ function Instrument_History_Card() {
                                 </div>
 
                                 <div className="row">
-                                    <div className=" col-md-3 row g-2 mb-3">
-                                        <div className="col"><Button>Report</Button></div>
-                                        <div className="col"><Button>Excel</Button></div>
-                                        <div className="col"><Button>Close</Button></div>
+                                    <div className=" col-md-3  g-2 mb-3 d-flex justify-content-start">
+                                        <div ><Button>Report</Button></div>
+                                        <div ><Button>Excel</Button></div>
+                                        <div ><Button>Close</Button></div>
                                     </div>
                                     <div className="col"></div>
-                                    <div className="col-md-8 row g-2 d-flex justify-content-end">
-                                        <div className="col"><Button>View Certificates</Button></div>
-                                        <div className="col"><Button>View Instructions</Button></div>
-                                        <div className="col"><Button>View Drawing</Button></div>
-                                        <div className="col"><Button>View R&R</Button></div>
-                                        <div className="col"><Button>View Attachment</Button></div>
+                                    <div className="col-md-8  g-2 d-flex justify-content-between">
+                                        <div ><Button>View Certificates</Button></div>
+                                        <div ><Button>View Instructions</Button></div>
+                                        <div ><Button>View Drawing</Button></div>
+                                        <div ><Button>View R&R</Button></div>
+                                        <div ><Button>View Attachment</Button></div>
                                     </div>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12">
-                                    <table className="table">
+                                    <table className="table table-bordered text-center align-middle">
                                         <thead>
                                             <tr>
                                                 <th>Serial No</th>
