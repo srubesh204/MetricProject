67dc2664b74dd01fdc5d55bf9841efcc5da6e761
srubesh204 <srubesh204@gmail.com>
2024-03-02 12:28:22 +0530

    g

--- client/src/Components/Reports/InsHistoryCard.jsx
+++ client/src/Components/Reports/InsHistoryCard.jsx
@@ -323,7 +323,7 @@ function InsHistoryCard() {
             align: 'center',
             headerAlign: 'center',
             renderCell: (params) => (
-                params.row.itemCalibrationSource === 'inhouse' && params.row.itemCertificateNo ? (
+                params.row.itemCalibratedAt === 'inhouse' && params.row.itemCertificateNo ? (
                     <IconButton size="small" component={Link} target="_blank" to={`${process.env.REACT_APP_PORT}/calCertificates/${params.row.itemCertificateNo}.pdf`}>
                         <FileCopy />
                     </IconButton>
